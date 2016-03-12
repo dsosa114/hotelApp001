9e400ed528d843a34ffd32798d960205007edc8a
Your name <you@example.com>
2016-03-12 15:10:36 -0600

    Falto un signo de = en mc

--- js/API/media-capture.js
+++ js/API/media-capture.js
@@ -1,10 +1,10 @@
-var mc{
+var mc = {
 	exito: function(mediaFiles){
 		var i, path, len;
     	for (i = 0, len = mediaFiles.length; i < len; i += 1) {
         	path = mediaFiles[i].fullPath;
         	// do something interesting with the file
-        	$("#fotoTomada").html('<img src="'+ path + '"');
+        	$("#fotoTomada").html('<img src="'+ path + '" style="width:100%;">');
         	$("#fotoTomada").attr("rel",path);
     	}
 	}, 
