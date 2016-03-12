323c2a42da8a7ada603e9fbecd72a166fadf6593
Your name <you@example.com>
2016-03-12 14:50:29 -0600

    Cambios a actions.js

--- js/actions.js
+++ js/actions.js
@@ -32,19 +32,19 @@ var fn = {
 		var foto = $("#fotoTomada").attr("rel");
 		try{
 			if(typeof nombre !== "string"){
-				throw new error("El nombre no es válido");
+				throw new Error("El nombre no es válido");
 			}
 			if(email == ""){
-				throw new error("Debe agregar email");
+				throw new Error("Debe agregar email");
 			}
 			if(foto == undefined){
-				throw new error("El usuario debe tomar una foto")
+				throw new Error("El usuario debe tomar una foto")
 			}
 			if(Number.isNaN(Number(tel))){
-				throw new error("El teléfono no es válido");
+				throw new Error("El teléfono no es válido");
 			}
-			if(tel.length == 10){
-				throw new error("El teléfono debe tener 10 digitos");
+			if(tel.length != 10){
+				throw new Error("El teléfono debe tener 10 digitos");
 			}
 
 			//Enviar el registro al servidor
